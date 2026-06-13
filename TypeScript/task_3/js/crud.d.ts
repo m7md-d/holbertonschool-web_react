51228397731d09746a7dd75a7720583efd17c04d
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-06-13 22:16:03 +0300

    solve task 8

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,7 @@
+// ========== Task 8: Ambient Namespaces - Type Declarations ==========
+
+import { RowID, RowElement } from './interface';
+
+declare function insertRow(row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
---
+++ TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// ========== Task 8: Ambient Namespaces - Interface & Type ==========
+
+export type RowID = number;
+
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
